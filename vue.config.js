a4c6763996ed452271648d0f7d0b4f2da8347b7f
niuma_semu <1479564558@qq.com>
2024-12-30 14:34:26 +0800

    优化打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 去除Vue打包后js目录下生成的一些.map文件，用于加速生产环境构建。
+  productionSourceMap: false,
+  // 去除Vue打包后.css和.js文件名称中8位hash值，跟缓存有关；一般为true就行。
+  filenameHashing:false
 })
